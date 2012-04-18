2b09d4fdbfe9938c5d8edde6d807bafb9de3aff5
Sef Kloninger <sef@kloninger.com>
2012-04-18 15:04:35 -0700

    Move the pi constant into its own file for cleanliness

---
+++ rpnCalcConstants.h
@@ -0,0 +1,11 @@
+//
+//  rpnCalcConstants.h
+//  rpnCalc
+//
+//  Created by Sef Kloninger on 4/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+const double pi;
